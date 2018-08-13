44d60df7a527c9a6289c4f33042282a7f5f3d7e0
vruvora <vvora8@gatech.edu>
2018-08-12 23:19:17 -0700

    dont render screen for tests

--- tests/random_agent.py
+++ tests/random_agent.py
@@ -27,9 +27,6 @@ if __name__ == '__main__':
     # directory, including one with existing data -- all monitor files
     # will be namespaced). You can also dump to a tempdir if you'd
     # like: tempfile.mkdtemp().
-    outdir = '/tmp/random-agent-results'
-    env = wrappers.Monitor(env, directory=outdir, force=True)
-    env.seed(0)
     agent = RandomAgent(env.action_space)
 
     episode_count = 100
@@ -46,6 +43,6 @@ if __name__ == '__main__':
             # Note there's no env.render() here. But the environment still can open window and
             # render if asked by env.monitor: it calls env.render('rgb_array') to record video.
             # Video is not recorded every episode, see capped_cubic_video_schedule for details.
-
+            print(f"Reward: {reward}")
     # Close the env and write monitor result info to disk
     env.env.close()
